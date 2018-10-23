1a716e5754585f5b78f145a5742030fb355ba0e7
Gabriel Sim <gabrielsim1993@gmail.com>
2018-10-23 19:30:19 +0800

    add therapist view

--- subsystem2/webapp/urls.py
+++ subsystem2/webapp/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 	url(r'^account/otp/verify/$', views.verify_otp),
 	url(r'^account/otp/challenge/$', views.challenge_view),
 	url(r'^account/logout/$', views.logout_view),
+
+	# Patient Views
 	url(r'^patient/index/$', views.patient_index_view, name='patient_index'),
 	url(r'^patient/index/(?P<type>\d+)/$', views.patient_index_view, name='patient_index'),
 	url(r'^patient/record/(?P<record_id>\d+)$', views.patient_record_view),
@@ -14,7 +16,13 @@ urlpatterns = [
 	url(r'^patient/record/(?P<record_id>\d+)/permission/(?P<therapist_id>\d+)/$', views.patient_file_permisison_detail_view, name='patient_file_permission_detail'),
 	url(r'^patient/permission/$', views.patient_permission_view, name='patient_perm'),
 	url(r'^patient/permission/(?P<therapist_id>\d+)/$', views.patient_permission_detail_view, name='patient_perm'),
-	url(r'^therapist/upload$', views.therapist_upload_data, name='therapist_upload_data'),
 
+	# Therapist Views
+	url(r'^therapist/index/$', views.therapist_index_view, name='therapist_index'),
+	url(r'^therapist/index/(?P<patient_id>\d+)$', views.therapist_index_view),
+	url(r'^patient/(?P<patient_id>\d+)/detail$', views.patient_detail_view),
+	url(r'^therapist/patient/(?P<patient_id>\d+)/$', views.therapist_list_patient_record_view),
+	url(r'^therapist/patient/(?P<patient_id>\d+)/(?P<type>\d+)$', views.therapist_list_patient_record_view),
+	url(r'^therapist/upload$', views.therapist_upload_data, name='therapist_upload_data'),
 	url(r'^keygen/', views.keygen_view),
 ]
