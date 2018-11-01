ff354ee7b17f0c971e0952f75be12122feb01b3c
amon <amon@nandynarwhals.org>
2018-11-02 01:50:28 +0800

    Add additional health data fields

--- subsystem2/webapp/forms.py
+++ subsystem2/webapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import IsAPatientOf, Patient, DATA_TYPES
+from .models import IsAPatientOf, Patient, HealthData
 from django.db.models import Count, Sum, Q
 from ckeditor.widgets import CKEditorWidget
 from django.core.exceptions import ValidationError
@@ -22,7 +22,7 @@ class UploadDataForm(forms.Form):
             self.fields['patient'].queryset = Patient.objects.filter(isapatientof__therapist__id=therapist_id)
 
     patient = forms.ModelChoiceField(queryset=None)
-    data_type = forms.ChoiceField(choices=DATA_TYPES)
+    data_type = forms.ChoiceField(choices=HealthData.DATA_TYPES)
     description = forms.CharField(widget=CKEditorWidget())
     file = forms.FileField(validators=[validate_file_size])
 
@@ -32,7 +32,7 @@ class UploadDataForm(forms.Form):
 
 
 class UploadPatientDataForm(forms.Form):
-    data_type = forms.ChoiceField(choices=DATA_TYPES)
+    data_type = forms.ChoiceField(choices=HealthData.DATA_TYPES)
     file = forms.FileField(validators=[validate_file_size])
 
     data_type.widget.attrs = {'class':'form-control'}
